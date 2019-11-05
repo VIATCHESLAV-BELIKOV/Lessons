f3d7b219d2bb7a622c541017ab47ed3de2c5b8b8
V Belikov <belikoff@hotmail.com>
2019-11-05 04:09:01 +0300

    Lesson 5 5/11/2019

--- src/main/java/com/ifmo/lesson5/Shapes.java
+++ src/main/java/com/ifmo/lesson5/Shapes.java
@@ -1,8 +1,23 @@
 package com.ifmo.lesson5;
 
+import com.ifmo.lesson5.LinkedList;
+
 public class Shapes {
     public static void main(String[] args) {
-        double totalArea = totalArea(
+        LinkedList shapesList = new LinkedList();
+        shapesList.add(new Rectangle(10, 15));
+        shapesList.add(new Rectangle(4, 8));
+        shapesList.add(new Square(23));
+        shapesList.add(new Triangle(5,10,13));
+        shapesList.add(new Triangle(7,17,22));
+        shapesList.add(new Oval(5,4));
+        shapesList.add(new Circle(10));
+
+        double totalArea1 = totalArea(shapesList);
+
+        System.out.println(totalArea1);
+
+        double totalArea2 = totalArea(
                 new Rectangle(10, 15),
                 new Rectangle(4, 8),
                 new Square(23),
@@ -10,7 +25,8 @@ public class Shapes {
                 new Oval(5,4),
                 new Circle(10));
 
-         System.out.println(totalArea);
+        System.out.println(totalArea2);
+
     }
 
     public static double totalArea(Shape... shapes) {
@@ -22,4 +38,16 @@ public class Shapes {
 
         return sum;
     }
+
+    public static double totalArea(LinkedList shapes) {
+        double sum = 0;
+        Shape shp;
+        while ((shp = (Shape)shapes.get(0)) != null)
+        {
+            sum += shp.area();
+            shapes.remove(0);
+        }
+
+        return sum;
+     }
 }
