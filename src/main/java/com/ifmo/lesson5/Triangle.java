75d551df8056527b1f4043f19670459753933e79
V Belikov <belikoff@hotmail.com>
2019-11-05 03:20:21 +0300

    Lesson 5 5/11/2019

--- src/main/java/com/ifmo/lesson5/Triangle.java
+++ src/main/java/com/ifmo/lesson5/Triangle.java
@@ -16,7 +16,7 @@ public class Triangle extends Shape {
     public double area() {
         double pp = (a + b + c)/2;
         double result = Math.sqrt(pp*(pp-a)*(pp-b)*(pp-c));
-        System.out.printf("rectangle(square) area a=%.0f, b=%.0f, c=%.0f is %.4f\n", a, b, c, result);
+        System.out.printf("triangle area a=%.0f, b=%.0f, c=%.0f is %.4f\n", a, b, c, result);
         return result;
     }
 }
