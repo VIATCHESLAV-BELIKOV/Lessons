45c6ad3c610c1a512c40946bb982977a3740fa19
Viatcheslav Belikov <Belikoff@hotmail.com>
2019-11-16 16:40:09 +0300

    16/11/2019

--- src/main/java/com/ifmo/lesson2/Fibonacci.java
+++ src/main/java/com/ifmo/lesson2/Fibonacci.java
@@ -12,7 +12,7 @@ public class Fibonacci {
         f[0] = 1;
         f[1] = 1;
         for (int i = 2; i < 11; ++i) f[i] = f[i - 1] + f[i - 2];
-        for (int i = 0; i < 11; i++) System.out.printf("%d",f[i]);
+        for (int i = 0; i < 11; i++) System.out.printf( "%d", f[i] );
         }
 
 }
