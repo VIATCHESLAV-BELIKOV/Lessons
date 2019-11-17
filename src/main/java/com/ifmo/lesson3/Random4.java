62e472449ef03c1281fe82725d420b9966b78641
Viatcheslav Belikov <Belikoff@hotmail.com>
2019-11-17 04:39:48 +0300

    17/11/2019

--- src/main/java/com/ifmo/lesson3/Random4.java
+++ src/main/java/com/ifmo/lesson3/Random4.java
@@ -1,5 +1,7 @@
 package com.ifmo.lesson3;
 
+import java.util.Random;
+
 public class Random4 {
     /*
     Создайте массив из 4 случайных целых чисел из отрезка [10;99], выведите его на экран в
@@ -10,12 +12,17 @@ public class Random4 {
         int[] randomNumbers = randomNumbers();
 
         // TODO implement
+        for (int i = 0; i < randomNumbers.length; i ++) System.out.printf("%d ", randomNumbers[i]);
+        System.out.println("");
+        
     }
 
     public static int[] randomNumbers() {
         // TODO implement
-
-        return new int[0];
+        int[] iArray = new int[4];
+        Random r = new Random();
+        for (int i = 0; i < iArray.length; i ++) iArray[i] = r.nextInt(89) + 10;
+        return iArray;
     }
 
     public static boolean isIncreasingSequence(int[] randomNumbers) {
