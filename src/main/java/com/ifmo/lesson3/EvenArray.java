44cbec29a87f24472e4961d75f8ee6de3254d3b0
Viatcheslav Belikov <Belikoff@hotmail.com>
2019-11-19 00:07:57 +0300

    18/11/2019

--- src/main/java/com/ifmo/lesson3/EvenArray.java
+++ src/main/java/com/ifmo/lesson3/EvenArray.java
@@ -1,5 +1,7 @@
 package com.ifmo.lesson3;
 
+import java.util.Random;
+
 public class EvenArray {
     /*
     Создайте массив из всех чётных чисел от 2 до 20 и выведите элементы массива на экран
@@ -11,11 +13,23 @@ public class EvenArray {
         int[] evenArray = evenArray();
 
         // TODO implement
+
+        for (int i = 0; i < evenArray.length; i ++) System.out.printf( "%d ", evenArray[i] );
+        System.out.println("");
+        for (int i = 0; i < evenArray.length; i ++) System.out.printf( "%d\n", evenArray[i] );
+
     }
 
     public static int[] evenArray() {
         // TODO implement
-
-        return new int[0];
+        int[] iArray = new int[(20-2)/2+1];
+        int j = 0;
+        Random r = new Random();
+        for (int i = 2; i <= 20; i ++)
+            if ( (i % 2 ) == 0 ) {
+                iArray[j] = i;
+                j++;
+            }
+        return iArray;
     }
 }
