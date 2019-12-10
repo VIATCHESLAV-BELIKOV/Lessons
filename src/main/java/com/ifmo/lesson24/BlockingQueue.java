d53d7b8122c94f2220678f5365dee03467231f2c
V Belikov <belikoff@hotmail.com>
2019-12-10 20:27:20 +0300

    Lesson 24 10/12/2019

--- src/main/java/com/ifmo/lesson24/BlockingQueue.java
+++ src/main/java/com/ifmo/lesson24/BlockingQueue.java
@@ -6,22 +6,32 @@ import java.util.Queue;
 public class BlockingQueue<T> {
 
     private Queue<T> queue = new LinkedList<T>();
+    private String qName;
     private int cN;
 
-    public BlockingQueue(int cN) {
+    public BlockingQueue(int cN, String qName) {
         this.cN = cN;
+        this.qName = qName;
     }
 
-    public synchronized void put(T element) throws InterruptedException {
-        while(queue.size() == cN) wait();
-        queue.add(element);
-        notify();
+    public synchronized void put(T Item) throws InterruptedException {
+        while(queue.size() == cN) {
+            System.out.printf("Очередь %s заполнена, Ждем ...\n", this.qName);
+            wait();
+        }
+        queue.add(Item);
+        System.out.printf("Добавляем элемент %s в очередь %s" + Item.toString(), this.qName );
+        notifyAll();
     }
 
     public synchronized T take() throws InterruptedException {
-        while(queue.isEmpty()) wait();
+        while (queue.isEmpty()){
+            System.out.printf("Очередь %s пуста, Ждем ...\n", this.qName);
+            wait();
+        }
         T item = queue.remove();
-        notify();
+        System.out.printf("Удаляем элемент %s из очереди %s" + item.toString(), this.qName );
+        notifyAll();
         return item;
     }
 }
