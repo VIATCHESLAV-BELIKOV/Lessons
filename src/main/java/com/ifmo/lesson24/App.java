588b51c615b3b2c5469c61dd0fe37183299f5900
V Belikov <belikoff@hotmail.com>
2019-12-10 19:39:34 +0300

    Lesson 24 10/12/2019

--- src/main/java/com/ifmo/lesson24/App.java
+++ src/main/java/com/ifmo/lesson24/App.java
@@ -4,9 +4,8 @@ import java.io.IOException;
 
 public class App {
 
-    public static void main(String[] args) throws IOException {
-
-
+    public static void main(String[] args) throws IOException  {
+        
     }
 
 }
